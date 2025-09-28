aff00b2366b002f083f11fba90f2ec7c961ed5b1
rileyburns707 <rileyburns707@gmail.com>
2025-09-27 21:29:26 -0400

    playing in the sandbox

--- calculator/sandbox.py
+++ calculator/sandbox.py
@@ -1,7 +1,6 @@
 """
-This is program generates a GUI for a simple calculator.
-The calculator functions just as a normal calculator 
-would the user just has to click the buttons.
+This is program is a sandbox for me to try different tkinter
+commands and learning how it works.
 """
 
 # Import tkinter
@@ -15,20 +14,36 @@ root.title("Test window title")
 # Set geometry of window (width x height)
 root.geometry('500x400')
 
+
+# adding a menu bar in the root window
+# new item in menu bar labelled as 'New'
+# adding more items in the mneu bar
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+item.add_command(label='Test')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+
+
 # adding a label to the root window
-label = Label(root, text = "Hello world!")
+label = Label(root, text = "What is 5*5?")
 label.grid()
 
-# function to display text when the button is clicked
+# adding Entry field
+text = Entry(root, width=8)
+text.grid(column=1, row=0)
 
-def testFunction():
-    label.configure(text = "Goodbye world")
+# function to display text when the button is clicked
+def clicked():
+    ans = "You answered: " + text.get()
+    label.configure(text = ans)
 
 # actual button widget
 button = Button(root, text = "Press here",
-                fg="red", bg="black", command = testFunction)
+                fg="blue", command = clicked)
 # set button grid
-button.grid(column=1, row=0)
+button.grid(column=2, row=0)
 
 # Execute Tkinter
 root.mainloop() # endless loop to make the window run till we close it
